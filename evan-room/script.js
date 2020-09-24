c8909a4aac4f63a0741ff2fd92fd1b2ed9824516
Evan Bird <edukusa@gmail.com>
2020-09-23 20:27:04 -0500

    evan-room first commit

---
+++ evan-room/script.js
@@ -0,0 +1,13 @@
+// let moonphases = document.getElementsByClassName("moonphase")
+// let links = ["https://edb394.github.io/abc-student-repo/projects/coding-foundation/","","","","","","","","",""]
+//
+// for (var i = 0; i < moonphases.length; i++) {
+//   moonphases[i].addEventListener("click", ()=>{
+//     let link = links[i]
+//     window.open(link)
+//   })
+// }
+
+document.getElementsByClassName("moonphase")[0].addEventListener("click",()=>{
+  window.open("https://edb394.github.io/abc-student-repo/projects/coding-foundation/")
+})
