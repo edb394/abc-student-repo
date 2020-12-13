5fe8d16f72608d57bbe18dea5a12bcbca1c1bb31
Evan <edb394@nyu.edu>
2020-12-13 21:17:03 +0800

    room mini update

--- evan-room/script.js
+++ evan-room/script.js
@@ -7,28 +7,47 @@
 //     window.open(link)
 //   })
 // }
-
-document.getElementsByClassName("moonphase")[0].addEventListener("click",()=>{
+let moonphase=document.getElementsByClassName("moonphase");
+moonphase[0].addEventListener("click",()=>{
   window.open("../projects/mini-project-1/")
 })
-document.getElementsByClassName("moonphase")[1].addEventListener("click",()=>{
+moonphase[1].addEventListener("click",()=>{
   window.open("../projects/mini-project-2/")
 })
-document.getElementsByClassName("moonphase")[2].addEventListener("click",()=>{
+moonphase[2].addEventListener("click",()=>{
   window.open("../projects/mini-project-3/")
 })
-document.getElementsByClassName("moonphase")[3].addEventListener("click",()=>{
+moonphase[3].addEventListener("click",()=>{
   window.open("../projects/project-a/")
 })
-document.getElementsByClassName("moonphase")[4].addEventListener("click",()=>{
+moonphase[4].addEventListener("click",()=>{
   window.open("../projects/bug-project/")
 })
-document.getElementsByClassName("moonphase")[5].addEventListener("click",()=>{
+moonphase[5].addEventListener("click",()=>{
   window.open("../projects/gift-project/")
 })
-document.getElementsByClassName("moonphase")[7].addEventListener("click",()=>{
+moonphase[7].addEventListener("click",()=>{
   window.open("https://acoustic-brassy-paint.glitch.me")
 })
-document.getElementsByClassName("moonphase")[8].addEventListener("click",()=>{
+moonphase[8].addEventListener("click",()=>{
   window.open("https://purple-working-enigmosaurus.glitch.me")
 })
+
+let moonvid=document.getElementById("moonvid");
+
+moonvid.onended=()=>{
+  console.log("ended");
+  moonvid.style.opacity="0%";
+  setTimeout(()=>{
+    moonvid.style.display="none";
+    for (var i = 0; i < moonphase.length; i++) {
+      moonphase[i].style.position="relative";
+    }
+  },1000);
+  for (var i = 0; i < document.getElementsByClassName("moonphasep").length; i++) {
+    document.getElementsByClassName("moonphasep")[i].style.opacity="100%";
+  }
+  for (var i = 0; i < document.getElementsByClassName("moonphaseimg").length; i++) {
+    document.getElementsByClassName("moonphaseimg")[i].style.opacity="100%";
+  }
+}
